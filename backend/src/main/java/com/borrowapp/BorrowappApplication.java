78b0f602bed26564941ca8d9a6be0204224e2120
Đoàn Anh Tuấn <tuanda.ptit@gmail.com>
2026-05-15 15:04:15 +0700

    init: setup backend Spring Boot base

---
+++ backend/src/main/java/com/borrowapp/BorrowappApplication.java
@@ -0,0 +1,13 @@
+package com.borrowapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrowappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrowappApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/borrowapp/BorrowappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borrowapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BorrowappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
